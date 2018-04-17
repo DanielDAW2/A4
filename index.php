05a1c039d6ada43f95e7aafe186a097ae88ac438
DanielDAW2 <rt.daniel.1994@gmail.com>
2018-04-17 18:58:38 +0200

    Comprovaciones de la capa de abstracción PDO

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+	
     require 'sys/DB.php';
     
     
@@ -7,22 +10,36 @@
 
    
     $db = DB::getInstance();
+
     
-    //$db->query("INSERT INTO `blog`.`Users` (`idusers`, `UserName`, `Pwd`, `Role`, `userscol`) VALUES ('4', 'ddsa', '123', '1', '1')");
+    /*$db->query('INSERT INTO `users` (`id`, `nombre`, `passwd`) VALUES (NULL, 'Daniel', '123')
+');
+    */
+    // $db->execute();
+
+    $db->query('SELECT * FROM users WHERE nombre = :nom');
+  
+    $db->bind(':nom','Daniel');    
     
-    $db->query('SELECT * FROM Users;');
-    
-    if($db->execute())
+    $db->execute();
+    echo '<h1>Var Dump de $db, que contiene el objeto PDO y la sentencia sin hacer bind()</h1>';
+    var_dump($db);
+
+    $rows = [];
+
+    $rows = $db->ResultSet();
+    echo '<h1>Resultado de la sentencia despues de hacer bind, donde :nom se convierte en "Daniel"</h1>';
+    if($rows)
     {
-        $rows = $db->ResultSet();
-        
         foreach($rows as $row)
         {
-            print $row['UserName'] . ' | ' . $row[Pwd] . '<br>';
+            echo "<p>".$row['nombre'] . " | " . $row['passwd'] . "</p>";       
         }
     }
-            
-    
-    $db->ResultSet();
 
+    echo '<h1>Single</h1><p>' . $db->single() . '</p>';
+
+    echo '<h1>Filas: </h1><p>'.$db->rowCount().'</p>';
+
+?>
     
